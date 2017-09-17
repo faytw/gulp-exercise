35d69c92ddcb0fc0038f81b68b2552f072eb99d9
Fay <rebecca.hsu1989@gmail.com>
2017-09-17 15:10:02 +0800

    change layout

--- src/script/main.js
+++ src/script/main.js
@@ -1,2 +1,21 @@
-console.log("******************");
-console.log("testing 123");
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        dotsEach: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
